7644b7b288dc5bafdd0af153dd8ac3c87aa03faf
jsoningram@gmail.com <jasoningram@Jasons-MacBook-Pro.local>
2016-06-28 16:01:10 -0700

    Adjust for Halogen

--- src/get_subscription_details.php
+++ src/get_subscription_details.php
@@ -1,44 +1,26 @@
 <?php
 	function get_subscription_details() {
-		$sweepstakes_trial_key = esc_attr( get_post_meta( $_POST[ 'postId' ], 'sweepstakes_trial_key', true ) ) 
-			? : 'two-week-free-trial'; 
-		$subscription_trial = null;
-		
-		if ( function_exists( 'load_slp_by_trial' ) ) {
-			$subscription_trial = load_slp_by_trial( $sweepstakes_trial_key );
-		}
+		$product_id = esc_attr( get_post_meta( 
+			$_POST[ 'postId' ], 'product_id', true ) ) 
+			? : 'Fallback'; 
 
-		$trial_period = 14;
+		$product_rate_plan_id = esc_attr( get_post_meta( 
+			$_POST[ 'postId' ], 'product_rate_plan_id', true ) ) 
+			? : 'Fallback'; 
 
-		require_once 'cls/common.php';
-		$services = new Services( $GLOBALS[ 'serverUrl' ] );
-		
-		$country = esc_attr( get_post_meta( $_POST[ 'postId' ], 'sweepstakes_country', true ) ) 
-			? : 'usa'; 
+		$product_rate_plan_charge_id = esc_attr( get_post_meta( 
+			$_POST[ 'postId' ], 'product_rate_plan_charge_id', true ) ) 
+			? : 'Fallback'; 
 
-		if ( '' == $country ) {
-			$geoLocation = new Common( $services );
-			$geoLocation->init( [ 'srv' => 'common', 'act' => 'getCountry' ] );
-			$country = $geoLocation->execPost();
-		}
-
-		if ( function_exists( 'load_promotion' ) ) {
-			$promotion          = load_promotion();
-			$subscription_price = $promotion[ 'promo_get_price_with_coupon' ];
-		} else {
-			$promotion = null;
-			$subscription_price = 'price_' . strtolower( $country );
-			$subscription_price = $subscription_trial[ 'subscription_details' ][ $subscription_price ];
-		}
-
-		$kaltura_id = 'kaltura_id_' . strtolower( $country );
-		$kaltura_id = $subscription_trial[ 'subscription_details' ][ $kaltura_id ];
+		$coupon = esc_attr( get_post_meta( 
+			$_POST[ 'postId' ], 'coupon', true ) ) 
+			? : 'Fallback'; 
 
 		$promo_details = [
-			'kalturaId'	        => $kaltura_id,
-			'country'           => $country,
-			'coupon'            => $promotion[ 'promotion_code' ],
-			'subscriptionPrice' => $subscription_price
+			'productId'	              => $product_id,
+			'productRatePlanId'       => $product_rate_plan_id,
+			'productRatePlanChargeId' => $product_rate_plan_charge_id,
+			'sweepstakesCoupon'       => $coupon
 		];
 
 		echo json_encode( $promo_details );
